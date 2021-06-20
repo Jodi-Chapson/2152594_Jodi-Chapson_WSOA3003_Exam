8b3654d7469ad475815d4fdb5d7ddc07948c3a48
Jodi Chapson <2152594@students.wits.ac.za>
2021-06-20 18:58:42 +0200

    Added basic entity sprites, basic player movement and cam follow

---
+++ Exam Submission/Assets/Scripts/CamController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    [Header("References")]
+    public Transform player;
+    public float Lerp;
+    private Vector3 CamPos;
+    public float CamY;
+    public bool CamFollow;
+
+
+    private void Start()
+    {
+        CamPos = transform.position;
+        CamFollow = true;
+
+    }
+
+    private void Update()
+    {
+
+
+
+        if (CamFollow)
+        {
+            CamPos = Vector3.Lerp(CamPos, player.position, Lerp);
+            CamPos = new Vector3(CamPos.x, CamPos.y + CamY, -15);
+            //CamPos.x = Mathf.Clamp(CamPos.x, -5, 34);
+            //CamPos.y = Mathf.Clamp(CamPos.y, -2, 7);
+
+            transform.position = CamPos;
+        }
+
+
+
+
+
+
+
+    }
+}
--- Exam Submission/Assets/Scripts/Player.cs
+++ Exam Submission/Assets/Scripts/Player.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("References")]
+    public bool canmove;
+    public Rigidbody2D rb;
+    public float Speed;
+    public float percentMod;
+
+
     void Start()
+    {
+        canmove = true;
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        if (canmove)
+        {
+            if (Input.GetKey("d"))
+            {
+                rb.velocity = new Vector2(Speed, rb.velocity.y);
+                //Flip();
+            }
+            else if (Input.GetKey("a"))
+            {
+                rb.velocity = new Vector2(-Speed, rb.velocity.y);
+
+                //Flip();
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+
+
+            if (Input.GetKey("w"))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, Speed * percentMod);
+            }
+            else if (Input.GetKey("s"))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -Speed * percentMod);
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+            }
+        }
     }
 }
